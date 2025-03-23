ed32026d7cc3a66018e76f965101074f65987d61
SearchingSteve <155838264+SearchingSteve@users.noreply.github.com>
2025-03-23 15:31:29 -0230

    Create JPA models
    JPA models created for member and tournament including getters & setters.

--- src/main/java/edu/keyin/stephencrocker/member/Member.java
+++ src/main/java/edu/keyin/stephencrocker/member/Member.java
@@ -1,4 +1,75 @@
 package edu.keyin.stephencrocker.member;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
 public class Member {
+    @Id
+    @SequenceGenerator(name = "member_sequence", sequenceName = "member_sequence", allocationSize = 1, initialValue=1)
+    @GeneratedValue(generator = "member_sequence")
+    private long id;
+    private String memberName;
+    private String memberAddress;
+    private String memberEmail;
+    private String memberPhone;
+    private LocalDate memberBirth;
+    private int membershipDuration;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getMemberName() {
+        return memberName;
+    }
+
+    public void setMemberName(String memberName) {
+        this.memberName = memberName;
+    }
+
+    public String getMemberAddress() {
+        return memberAddress;
+    }
+
+    public void setMemberAddress(String memberAddress) {
+        this.memberAddress = memberAddress;
+    }
+
+    public String getMemberEmail() {
+        return memberEmail;
+    }
+
+    public void setMemberEmail(String memberEmail) {
+        this.memberEmail = memberEmail;
+    }
+
+    public String getMemberPhone() {
+        return memberPhone;
+    }
+
+    public void setMemberPhone(String memberPhone) {
+        this.memberPhone = memberPhone;
+    }
+
+    public LocalDate getMemberBirth() {
+        return memberBirth;
+    }
+
+    public void setMemberBirth(LocalDate memberBirth) {
+        this.memberBirth = memberBirth;
+    }
+
+    public int getMembershipDuration() {
+        return membershipDuration;
+    }
+
+    public void setMembershipDuration(int membershipDuration) {
+        this.membershipDuration = membershipDuration;
+    }
 }
--- src/main/java/edu/keyin/stephencrocker/tournament/Tournament.java
+++ src/main/java/edu/keyin/stephencrocker/tournament/Tournament.java
@@ -1,4 +1,79 @@
 package edu.keyin.stephencrocker.tournament;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import edu.keyin.stephencrocker.member.Member;
+
+@Entity
 public class Tournament {
+    @Id
+    @SequenceGenerator(name = "tournament_sequence", sequenceName = "tournament_sequence", allocationSize = 1, initialValue=1)
+    @GeneratedValue(generator = "tournament_sequence")
+    private long id;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private String location;
+    private BigDecimal entryFee;
+    private BigDecimal cashPrize;
+    @ManyToMany
+    private List<Member> participatingMembers;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public BigDecimal getEntryFee() {
+        return entryFee;
+    }
+
+    public void setEntryFee(BigDecimal entryFee) {
+        this.entryFee = entryFee;
+    }
+
+    public BigDecimal getCashPrize() {
+        return cashPrize;
+    }
+
+    public void setCashPrize(BigDecimal cashPrize) {
+        this.cashPrize = cashPrize;
+    }
+
+    public List<Member> getParticipatingMembers() {
+        return participatingMembers;
+    }
+
+    public void setParticipatingMembers(List<Member> participatingMembers) {
+        this.participatingMembers = participatingMembers;
+    }
 }
