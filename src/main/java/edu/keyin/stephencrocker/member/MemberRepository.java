0808756d7d60d883266b2e37f9a7b17a0d1d5d76
SearchingSteve <155838264+SearchingSteve@users.noreply.github.com>
2025-03-23 18:03:01 -0230

    refactor: Change method naming in MemberRepository
    Change findByEmail
    to findByMemberEmail
    
    & findByPhoneNumberContaining
    to findByMemberPhoneContaining

--- src/main/java/edu/keyin/stephencrocker/member/MemberRepository.java
+++ src/main/java/edu/keyin/stephencrocker/member/MemberRepository.java
@@ -1,12 +1,13 @@
 package edu.keyin.stephencrocker.member;
 
-
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberRepository extends CrudRepository<Member, Long> {
     public Member findByMemberNameContainingIgnoreCase(String name);
-    public Member findByEmail(String email);
-    public Member findByPhoneNumberContaining(String phoneNumber);
+
+    public Member findByMemberEmail(String email);
+
+    public Member findByMemberPhoneContaining(String phoneNumber);
 }
